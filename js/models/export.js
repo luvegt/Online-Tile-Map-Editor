5693edbaac4b4d1979e6674007a97ff0e8427689
Elias Schütt <elias.schuett@gmail.com>
2013-06-24 01:48:47 +0200

    Updated export function

--- js/models/export.js
+++ js/models/export.js
@@ -16,16 +16,17 @@ define(["jquery-ui"], function($) {
 		$(".layer").each(function() {
 
 			layer = $(this).attr("data-name");
-			output[layer] = [];
+			output[layer] = {};
 
 			$(this).find("div").each(function() {
-				output[layer].push({
-					coords: $(this).attr("data-coords"),
-					tileset: $(this).attr("data-tileset"),
-				});
+				coords = $(this).attr("data-coords");
+				tileset = $(this).attr("data-tileset");
+		
+				if (!output[layer][tileset]) { output[layer][tileset] = []; }
+				output[layer][tileset].push(coords);
 			});
 		});
-
+		
 		window.open("data:text/json;charset=UTF-8;," + JSON.stringify(output), "_blank");
 	};
 
